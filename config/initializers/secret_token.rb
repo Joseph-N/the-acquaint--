4f51abbc72b74f4abb6e9e08745ecf11919882fe
Joseph-N <josendugu@hotmail.com>
2013-01-18 14:20:22 +0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Acquianter::Application.config.secret_token = 'ef3c5c20fe2b92a368cc6d6ff7dbf26caa941e5b0508059e5987bd0605a7be5d80ca81229fe359c3d57539e4675e1ec0ed85116829975ef44c9b6d01435b3fae'
