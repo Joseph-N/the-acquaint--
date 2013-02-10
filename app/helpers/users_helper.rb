d3a2703b37c3cc94f40964f4c3ac92a107d949ef
Joseph-N <josendungu@hotmail.com>
2013-02-10 15:57:40 +0300

    Code refactor & default image fix

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,6 +1,6 @@
 module UsersHelper
   
-  #returns default image for a given gender
+  #returns default large image for a given gender
   def gravator_for(user)
     if user.gender == "male"
       image_tag("user-male-icon.png")
@@ -8,4 +8,23 @@ module UsersHelper
       image_tag("user-female-icon.png")
     end
   end
+  
+  #returns a default small image for a given gender
+  def default_gender_image(user)
+    if user.gender == "male"
+      image_tag("default-male-icon.jpg")
+    else
+      image_tag("default-female-icon.jpg")
+    end    
+  end
+  
+  #returns the thubmail for a given user
+  def thumbnail_for(user)
+    if user.photos.any?
+      image_tag user.photos.first.image.url(:small)
+    else
+      default_gender_image(user)
+    end
+  end
+  
 end
