f41a9e67cebefb353d356babe3e94d5f0461d96c
Joseph-N <josendungu@hotmail.com>
2013-03-02 19:30:25 +0300

    Added default url for action_mailer

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,4 +64,7 @@ Acquianter::Application.configure do
   # Log the query plan for queries taking more than this (works
   # with SQLite, MySQL, and PostgreSQL)
   # config.active_record.auto_explain_threshold_in_seconds = 0.5
+
+  # Default email to send out notices
+  config.action_mailer.default_url_options = { :host => 'http://fathomless-falls-8042.herokuapp.com/' }
 end
