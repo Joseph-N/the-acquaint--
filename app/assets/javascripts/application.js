5294d18cab5e0947e4caee1334676f026a461aa3
Joseph-N <josendungu@hotmail.com>
2013-03-04 22:08:02 +0300

    added more to the UI

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,24 @@
 jQuery.ajaxSetup({
 	'beforeSend': function (xhr) {xhr.setRequestHeader("Accept", "text/javascript")}
 });
+
+// function to count remaining characters in a given text box
+(function($){
+    $.fn.countChars = function(){
+    	var totalChars      = 255; //Total characters allowed in textarea
+	    var countTextBox    = this; // Textarea input box
+	    var charsCountEl    = $('#count'); // Remaining chars count will be displayed here
+	    
+	    charsCountEl.text(totalChars); //initial value of countchars element
+	    countTextBox.keyup(function() { //user releases a key on the keyboard
+	        var thisChars = this.value.replace(/{.*}/g, '').length; //get chars count in textarea
+	        if(thisChars > totalChars) //if we have more chars than it should be
+	        {
+	            var CharsToDel = (thisChars-totalChars); // total extra chars to delete
+	            this.value = this.value.substring(0,this.value.length-CharsToDel); //remove excess chars from textarea
+	        }else{
+	            charsCountEl.text( totalChars - thisChars ); //count remaining chars
+	        }
+	    });
+    };
+})(jQuery);
